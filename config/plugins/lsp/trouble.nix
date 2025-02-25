0039b664cec0aeb8d91b32cb9836806ae460a346
spector700 <72362399+spector700@users.noreply.github.com>
2025-02-24 22:59:43 -0600

    fix(trouble): keybinds now worky

--- config/plugins/lsp/trouble.nix
+++ config/plugins/lsp/trouble.nix
@@ -1,49 +1,82 @@
 {
-  plugins.trouble = {
-    enable = true;
-    settings.auto_close = true;
+  plugins = {
+    trouble = {
+      enable = true;
+      settings = {
+        auto_close = true;
+        modes = {
+          preview_split = {
+            # NOTE: can automatically open when diagnostics exist
+            # auto_open = true;
+            mode = "diagnostics";
+            preview = {
+              type = "split";
+              relative = "win";
+              position = "right";
+              size = 0.5;
+            };
+          };
+        };
+      };
+    };
+
+    which-key.settings.spec = [
+      {
+        __unkeyed-1 = "<leader>x";
+        mode = "n";
+        icon = "";
+        group = "Trouble";
+      }
+    ];
   };
 
   keymaps = [
     {
       mode = "n";
-      key = "<leader>x";
-      action = "+diagnostics/quickfix";
-    }
-    {
-      mode = "n";
       key = "<leader>xx";
-      action = "<cmd>TroubleToggle<cr>";
+      action = "<cmd>Trouble preview_split toggle<cr>";
       options = {
-        silent = true;
-        desc = "Document Diagnostics (Trouble)";
+        desc = "Diagnostics toggle";
       };
     }
     {
       mode = "n";
       key = "<leader>xX";
-      action = "<cmd>TroubleToggle workspace_diagnostics<cr>";
+      action = "<cmd>Trouble preview_split toggle filter.buf=0<cr>";
       options = {
-        silent = true;
-        desc = "Workspace Diagnostics (Trouble)";
+        desc = "Buffer Diagnostics toggle";
       };
     }
     {
       mode = "n";
-      key = "<leader>xt";
-      action = "<cmd>TroubleToggle todo<cr>";
+      key = "<leader>us";
+      action = "<cmd>Trouble symbols toggle focus=false<cr>";
       options = {
-        silent = true;
-        desc = "Todo (Trouble)";
+        desc = "Symbols toggle";
       };
     }
     {
       mode = "n";
-      key = "<leader>xq";
-      action = "<cmd>TodoQuickFix<cr>";
+      key = "<leader>xl";
+      action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
       options = {
-        silent = true;
-        desc = "Quickfix List (Trouble)";
+        desc = "LSP Definitions / references / ... toggle";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>xL";
+      action = "<cmd>Trouble loclist toggle<cr>";
+      options = {
+        desc = "Location List toggle";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>xQ";
+      action = "<cmd>Trouble qflist toggle<cr>";
+      options = {
+        desc = "Quickfix List toggle";
       };
     }
   ];
