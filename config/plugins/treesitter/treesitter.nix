917f88b1c629b07b9e76dfe05d411d284906ba3f
spector700 <72362399+spector700@users.noreply.github.com>
2025-03-18 16:35:02 -0500

    feat(treesitter): add settings

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -1,14 +1,56 @@
+{ config, lib, ... }:
 {
   plugins = {
     treesitter = {
       enable = true;
+      folding = true;
+
       settings = {
-        highlight.enable = true;
+        highlight = {
+          additional_vim_regex_highlighting = true;
+          enable = true;
+          disable = # Lua
+            ''
+              function(lang, bufnr)
+                return vim.api.nvim_buf_line_count(bufnr) > 10000
+              end
+            '';
+        };
+
         incremental_selection.enable = true;
         indent.enable = true;
       };
       nixvimInjections = true;
     };
-    treesitter-context.enable = true;
+
+    treesitter-context = {
+      inherit (config.plugins.treesitter) enable;
+      settings = {
+        max_lines = 4;
+        min_window_height = 40;
+        multiwindow = true;
+        separator = "-";
+      };
+    };
+
+    treesitter-refactor = {
+      inherit (config.plugins.treesitter) enable;
+
+      highlightDefinitions = {
+        enable = true;
+        clearOnCursorMove = true;
+      };
+      smartRename.enable = true;
+      navigation.enable = true;
+    };
   };
+
+  keymaps = lib.mkIf config.plugins.treesitter-context.enable [
+    {
+      mode = "n";
+      key = "<leader>uT";
+      action = "<cmd>TSContextToggle<cr>";
+      options.desc = "Treesitter Context toggle";
+    }
+  ];
 }
