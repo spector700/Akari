6d7574e3c716133ce6ad97b6268f6b37987f0c2e
spector700 <72362399+spector700@users.noreply.github.com>
2025-04-26 15:20:20 -0500

    fix: add nixpkgs to be able to update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,11 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixvim.url = "github:nix-community/nixvim";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
