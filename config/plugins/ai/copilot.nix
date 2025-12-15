3bfa3db97f89b0fd9c98f9974ab10ff0098aef72
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-15 15:27:47 -0600

    fix: updated copilot config

--- config/plugins/ai/copilot.nix
+++ config/plugins/ai/copilot.nix
@@ -1,4 +1,20 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) optionals mkIf;
+in
+{
+  extraPlugins = optionals config.plugins.copilot-lua.enable (
+    with pkgs.vimPlugins;
+    optionals config.plugins.lualine.enable [
+      copilot-lualine
+    ]
+  );
+
   plugins = {
     copilot-lua = {
       enable = true;
@@ -8,10 +24,46 @@
       };
 
       settings = {
-        nes = {
-          move_count_threshold = 3;
+        nes = mkIf (!config.plugins.sidekick.enable) {
+          enabled = true;
+          keymap = {
+            accept_and_goto = "<TAB>";
+            accept = false;
+            dismiss = "<Esc>";
+          };
+        };
+
+        filetypes = {
+          yaml = false;
+          markdown = false;
+          json = false;
+          help = false;
+          gitcommit = false;
+          gitrebase = false;
         };
       };
     };
   };
+
+  autoCmd = mkIf (config.plugins.copilot-lua.enable && config.plugins.blink-cmp.enable) [
+    {
+      event = "User";
+      pattern = "BlinkCmpMenuOpen";
+      callback.__raw = ''
+        function()
+          vim.b.copilot_suggestion_hidden = true
+        end
+      '';
+    }
+
+    {
+      event = "User";
+      pattern = "BlinkCmpMenuClose";
+      callback.__raw = ''
+        function()
+          vim.b.copilot_suggestion_hidden = false
+        end
+      '';
+    }
+  ];
 }
