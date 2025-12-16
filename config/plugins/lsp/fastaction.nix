de48c9177e020c204ce8de6c4fdc643114ccb12a
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-15 19:14:37 -0600

    fix: change fastaction keybinds

--- config/plugins/lsp/fastaction.nix
+++ config/plugins/lsp/fastaction.nix
@@ -6,24 +6,24 @@
 
       lazyLoad.settings.lazy = true;
     };
-
-    keymaps = lib.mkIf config.plugins.fastaction.enable [
-      {
-        mode = "n";
-        key = "<leader>lc";
-        action = ''<cmd>lua require('fastaction').code_action()<cr>'';
-        options = {
-          desc = "Fastaction code action";
-        };
-      }
-      {
-        mode = "v";
-        key = "<leader>lc";
-        action = ''<cmd>lua require('fastaction').range_code_action()<cr>'';
-        options = {
-          desc = "Fastaction code action";
-        };
-      }
-    ];
   };
+
+  keymaps = lib.mkIf config.plugins.fastaction.enable [
+    {
+      mode = "n";
+      key = "<leader>lc";
+      action = ''<cmd>lua require('fastaction').code_action()<cr>'';
+      options = {
+        desc = "Fastaction code action";
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>lc";
+      action = ''<cmd>lua require('fastaction').range_code_action()<cr>'';
+      options = {
+        desc = "Fastaction code action";
+      };
+    }
+  ];
 }
