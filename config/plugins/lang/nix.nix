6bfcf25e87ce3b3a06d0a4a4d6207d34f3386f36
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-15 19:57:14 -0600

    feat(nix): switch to deadnix linter

--- config/plugins/lang/nix.nix
+++ config/plugins/lang/nix.nix
@@ -7,7 +7,7 @@
 {
   plugins = {
     nix.enable = true;
-    hmts.enable = true;
+    # hmts.enable = true;
     nix-develop.enable = true;
 
     conform-nvim.settings = {
@@ -18,34 +18,36 @@
 
     lint = {
       lintersByFt = {
-        nix = [ "statix" ];
+        nix = [ "deadnix" ];
       };
 
       linters = {
-        statix.cmd = lib.getExe pkgs.statix;
+        deadnix.cmd = lib.getExe pkgs.deadnix;
       };
     };
 
-    lsp.servers.nixd = {
-      enable = true;
-      settings =
-        let
-          flake = ''(builtins.getFlake "${self}")'';
-          system = ''''${builtins.currentSystem}'';
-        in
-        {
-          formatting = {
-            command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+    lsp.servers = {
+      nixd = {
+        enable = true;
+        settings =
+          let
+            flake = ''(builtins.getFlake "${self}")'';
+            system = ''''${builtins.currentSystem}'';
+          in
+          {
+            formatting = {
+              command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+            };
+            nixpkgs.expr = "import ${flake}.inputs.nixpkgs { }";
+            options = {
+              nixvim.expr = ''${flake}.packages.${system}.nvim.options'';
+              # NOTE: These will be passed in from outside using `.extend` from the flake installing this package
+              # nix-darwin.expr = ''${flake}.darwinConfigurations.khanelimac.options'';
+              # nixos.expr = ''${flake}.nixosConfigurations.khanelinix.options'';
+              # home-manager.expr = ''${nixos.expr}.home-manager.users.type.getSubOptions [ ]'';
+            };
           };
-          nixpkgs.expr = "import ${flake}.inputs.nixpkgs { }";
-          options = {
-            nixvim.expr = ''${flake}.packages.${system}.nvim.options'';
-            # NOTE: These will be passed in from outside using `.extend` from the flake installing this package
-            # nix-darwin.expr = ''${flake}.darwinConfigurations.khanelimac.options'';
-            # nixos.expr = ''${flake}.nixosConfigurations.khanelinix.options'';
-            # home-manager.expr = ''${nixos.expr}.home-manager.users.type.getSubOptions [ ]'';
-          };
-        };
+      };
     };
   };
 }
