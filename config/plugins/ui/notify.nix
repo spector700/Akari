9a61554f426f09e5fb6e4b07255e82f978fbbc45
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-19 17:22:55 -0600

    feat: updated noice and notify

--- config/plugins/ui/notify.nix
+++ config/plugins/ui/notify.nix
@@ -1,10 +1,8 @@
 {
-  plugins.notify = {
-    enable = true;
-    # remove animations for performance
-    settings = {
-      stages = "static";
-      timeout = 4000;
+  plugins = {
+    notify = {
+      enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
     };
   };
 
@@ -20,28 +18,4 @@
       };
     }
   ];
-
-  extraConfigLua = ''
-    local notify = require("notify")
-
-    local filtered_message = { "No information available" }
-
-    -- Override notify function to filter out messages
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.notify = function(message, level, opts)
-    	local merged_opts = vim.tbl_extend("force", {
-    		on_open = function(win)
-    			local buf = vim.api.nvim_win_get_buf(win)
-    			vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-    		end,
-    	}, opts or {})
-
-    	for _, msg in ipairs(filtered_message) do
-    		if message == msg then
-    			return
-    		end
-    	end
-    	return notify(message, level, merged_opts)
-    end
-  '';
 }
