f59d2373e09a929c9433cf25a8e8b6cb230edda8
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-20 20:08:30 -0600

    feat: lazyload and change keybinds for persistence

--- config/plugins/util/persistence.nix
+++ config/plugins/util/persistence.nix
@@ -3,31 +3,41 @@
   plugins = {
     persistence = {
       enable = true;
+
+      lazyLoad.settings.event = "BufReadPre";
     };
+
+    which-key.settings.spec = lib.optionals config.plugins.persistence.enable [
+      {
+        __unkeyed-1 = "<leader>S";
+        group = "Persistence";
+        icon = "󰘛";
+      }
+    ];
   };
 
   keymaps = lib.optionals config.plugins.persistence.enable [
     {
       mode = "n";
-      key = "<leader>ql";
+      key = "<leader>Sl";
       action.__raw = "function() require('persistence').load() end";
       options.desc = "Load the session for the current directory";
     }
     {
       mode = "n";
-      key = "<leader>qs";
+      key = "<leader>Ss";
       action.__raw = "function() require('persistence').select() end";
       options.desc = "Select a session to load";
     }
     {
       mode = "n";
-      key = "<leader>qL";
+      key = "<leader>SL";
       action.__raw = "function() require('persistence').load({ last = true }) end";
       options.desc = "Load the last session";
     }
     {
       mode = "n";
-      key = "<leader>qS";
+      key = "<leader>SS";
       action.__raw = "function() require('persistence').stop() end";
       options.desc = "Stop Persistence";
     }
