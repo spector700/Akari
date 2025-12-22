a53710f8bb1f0db874efd21ec8c28300672b532b
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-21 19:28:35 -0600

    feat: updated trouble nvim

--- config/plugins/lsp/trouble.nix
+++ config/plugins/lsp/trouble.nix
@@ -2,9 +2,22 @@
   plugins = {
     trouble = {
       enable = true;
+
+      lazyLoad.settings.cmd = [ "Trouble" ];
+
       settings = {
         auto_close = true;
         modes = {
+          diagnostics = {
+            filter.__raw = ''
+              function(items)
+                return vim.tbl_filter(function(item)
+                    return not string.match(item.basename, [[%__virtual.cs$]])
+                end, items)
+              end
+            '';
+          };
+
           preview_split = {
             # NOTE: can automatically open when diagnostics exist
             # auto_open = true;
@@ -14,6 +27,26 @@
               relative = "win";
               position = "right";
               size = 0.5;
+            };
+          };
+
+          preview_float = {
+            mode = "diagnostics";
+            preview = {
+              type = "float";
+              relative = "editor";
+              border = "rounded";
+              title = "Preview";
+              title_pos = "center";
+              position = [
+                0
+                (-2)
+              ];
+              size = {
+                width = 0.3;
+                height = 0.3;
+              };
+              zindex = 200;
             };
           };
         };
