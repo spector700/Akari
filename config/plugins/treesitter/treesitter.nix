f06b0762db4ebf2f930316c5059e51e6e4841d1c
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-21 19:19:26 -0600

    feat: updated treesiter

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -3,7 +3,33 @@
   plugins = {
     treesitter = {
       enable = true;
-      # folding = true;
+      # folding.enable = true;
+
+      grammarPackages =
+        let
+          # Large grammars that are not used
+          excludedGrammars = [
+            "agda-grammar"
+            "cuda-grammar"
+            "d-grammar"
+            "fortran-grammar"
+            "gnuplot-grammar"
+            "haskell-grammar"
+            "hlsl-grammar"
+            "julia-grammar"
+            "koto-grammar"
+            "lean-grammar"
+            "nim-grammar"
+            "scala-grammar"
+            "slang-grammar"
+            "systemverilog-grammar"
+            "tlaplus-grammar"
+            "verilog-grammar"
+          ];
+        in
+        lib.filter (
+          g: !(lib.elem g.pname excludedGrammars)
+        ) config.plugins.treesitter.package.passthru.allGrammars;
 
       settings = {
         highlight = {
@@ -17,7 +43,15 @@
             '';
         };
 
-        incremental_selection.enable = true;
+        incremental_selection = {
+          enable = true;
+          keymaps = {
+            init_selection = "<A-o>";
+            node_incremental = "<A-o>";
+            scope_incremental = "<A-O>";
+            node_decremental = "<A-i>";
+          };
+        };
         indent.enable = true;
       };
       nixvimInjections = true;
@@ -25,6 +59,8 @@
 
     treesitter-context = {
       inherit (config.plugins.treesitter) enable;
+      lazyLoad.settings.event = "DeferredUIEnter";
+
       settings = {
         max_lines = 4;
         min_window_height = 40;
@@ -35,13 +71,18 @@
 
     treesitter-refactor = {
       inherit (config.plugins.treesitter) enable;
-      settings = {
 
+      settings = {
         highlightDefinitions = {
           enable = true;
           clearOnCursorMove = true;
         };
-        smartRename.enable = true;
+        smartRename = {
+          enable = true;
+          keymaps = {
+            smart_rename = "gR";
+          };
+        };
         navigation.enable = true;
       };
     };
@@ -50,8 +91,8 @@
   keymaps = lib.mkIf config.plugins.treesitter-context.enable [
     {
       mode = "n";
-      key = "<leader>uT";
-      action = "<cmd>TSContextToggle<cr>";
+      key = "<leader>ut";
+      action = "<cmd>TSContext toggle<cr>";
       options.desc = "Treesitter Context toggle";
     }
   ];
