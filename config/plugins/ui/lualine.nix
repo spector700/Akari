83e0a1a402e5b96363c34f93e7bdc7535375b8d3
spector700 <72362399+spector700@users.noreply.github.com>
2025-12-25 12:30:41 -0600

    fix(lualine): add profiler

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -192,8 +192,9 @@ in
                 removed= "${icons.git.LineRemoved}",
                 },
               },
-              -- TODO: fix this
-              -- { Snacks.profiler.status() },
+              {
+                "Snacks.profiler.status()",
+              },
               ${optionalString (config.plugins.sidekick.enable) ''
                 {
                   function()
@@ -217,25 +218,11 @@ in
                   end,
                 },
               ''}
-              {
-                function()
-                    local msg = ""
-                    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                    local clients = vim.lsp.get_clients()
-                    if next(clients) == nil then
-                        return msg
-                    end
-                  for _, client in ipairs(clients) do
-                      local filetypes = client.config.filetypes
-                      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                          return client.name
-                      end
-                  end
-                  return msg
-                end,
-                icon = " ",
-              },
-              ${optionalString (config.plugins.copilot-lua.enable) "copilot,"}
+              ${optionalString (config.plugins.copilot-lua.enable) ''
+                {
+                  "copilot",
+                }
+              ''}
           }
       }
     })
